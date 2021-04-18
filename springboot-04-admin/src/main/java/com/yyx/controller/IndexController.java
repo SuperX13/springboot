108dcd4eed0a4e9655a757b944d03ff09b3291d6
yongxin <965224591@qq.com>
2021-04-18 22:28:56 +0800

    springboot学习

---
+++ springboot-04-admin/src/main/java/com/yyx/controller/IndexController.java
@@ -0,0 +1,40 @@
+package com.yyx.controller;
+
+import com.yyx.pojo.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class IndexController {
+    @GetMapping(value = {"/","/login"})
+    public String loginPage(){
+        return "login";
+    }
+    @PostMapping("/login")
+    public String index(User user, HttpSession httpSession, Model model){
+        if ("admin".equals(user.getUserName()) && "123".equals(user.getPassword())){
+            httpSession.setAttribute("user",user);
+            //登录成功重定向到首页，防止重复提交表单
+            return "redirect:/toIndex";
+        }else {
+            model.addAttribute("msg","账号密码错误");
+            return "login";
+        }
+    }
+    @GetMapping("/toIndex")
+    public String indexPage(HttpSession httpSession,Model model){
+        Object user = httpSession.getAttribute("user");
+        if (user !=null){
+            return "index";
+        }else {
+            model.addAttribute("msg","请先登录");
+            return "login";
+        }
+
+    }
+}
